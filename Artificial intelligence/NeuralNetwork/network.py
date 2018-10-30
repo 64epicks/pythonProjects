6c4e51b3186cc99a6b24bf129b8fc21031e317d6
AlastairP <alastairp@alastairp.ddns.net>
2018-10-30 10:18:12 +0100

    Coding

--- Artificial intelligence/NeuralNetwork/network.py
+++ Artificial intelligence/NeuralNetwork/network.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-class NeuralNetwork:
-    def __init__(self, layers):
+def sigmoid(x):
+  return 1.0 / (1 + np.exp(-x))
+
+class Network:
+    def __init__(self, layers, layerLengths):
+        self.biases = np.zeros((layers, 4))
+        print(self.biases)
+        #self.weights = np.random.rand(layers, 1, 1)
+        for i in range(layers):
+            self.biases[i].shape = (layerLengths[i])
+            self.biases[i] = np.random.rand(layerLengths[i])
+            self.weights[i] = np.random.rand(layerLengths[i], layerLengths[i - 1])
+        self.output = np.zeros(layerLengths[len(layerLengths) - 1])
+
+    def executeNetwork(self, x):
+        self.activations[0] = x
+        for i in range(len(self.weights))[0:]:
+            self.activations[i] = sigmoid(np.dot(self.activations[i - 1], self.weights[i]))
+        return self.activations
---
+++ Artificial intelligence/NeuralNetwork/program.py
@@ -0,0 +1,4 @@
+import numpy as np
+import network as nw
+
+nn = nw.Network(3, np.array([3, 4, 4]))
